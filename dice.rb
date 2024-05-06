0aa7849f1f2eb95fef01a55b9f081277cdfa6ef3
Candide Mboungou-Kimpolo <89795037+Candide23@users.noreply.github.com>
2024-05-06 16:35:15 +0000

    assignment done

--- dice.rb
+++ dice.rb
@@ -26,46 +26,72 @@ end
 
 
 get("/dice/2/6") do
-  first_die = rand(1..6)
-  second_die = rand(1..6)
-  sum = first_die + second_die
+  @rolls = []
 
-  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+  2.times do
+    die = rand(2..6)
 
+    @rolls.push(die)
+  end
+
+	
   erb(:two_six)
+
 end
 
 
 
 get("/dice/2/10") do
-  first_die = rand(2..10)
-  second_die = rand(2..10)
-  sum = first_die + second_die
-	
-  @outcome = "You rolled a #{first_die} and a #{second_die} "
+  @rolls = []
+
+  2.times do
+    die = rand(2..20)
+
+    @rolls.push(die)
+  end
+
 	
   erb(:two_ten)
-
 end
+
+
+
 
 get("/dice/1/20") do
-  first_die = rand(1..10)
-  second_die = rand(1..10)
-  sum = first_die + second_die
-	
-  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
-	
-  erb(:one_twenty)
+  @rolls = []
 
+  1.times do
+    die = rand(1..20)
+
+    @rolls.push(die)
+  end
+
+  erb(:one_twenty)
 end
 
-get("/dice/5/4") do
-  first_die = rand(5..4)
-  second_die = rand(5..4)
-  sum = first_die + second_die
-	
-  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
-	
-  erb(:five_four)
 
+get("/dice/5/4") do
+  @rolls = []
+
+  5.times do
+    die = rand(5..4)
+
+    @rolls.push(die)
+  end
+
+  erb(:five_four)
+end
+
+
+
+get("/dice/100/6") do
+  @rolls = []
+
+  100.times do
+    die = rand(1..6)
+
+    @rolls.push(die)
+  end
+
+  erb(:one_hundred_six)
 end
